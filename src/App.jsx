54b616efeeeaa96bc46b48cff5d61596e6c6d752
Sanjiv Jha <narutosanjiv@gmail.com>
2022-06-12 03:17:21 +0530

    Added first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Aboutus from './components/about'
+
+function App() {
+
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <Aboutus />
+    </main>
+  )
+}
+
+export default App
